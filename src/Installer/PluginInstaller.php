eef7f489a779eb9a8f2866a3e038acfde5b4ae0f
Bryan Crowe <bryan@bryan-crowe.com>
2015-01-02 18:19:40 -0500

    Run phpcbf for PSR2 CS fixers

--- src/Installer/PluginInstaller.php
+++ src/Installer/PluginInstaller.php
@@ -4,62 +4,64 @@ namespace Cake\Composer\Installer;
 use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
 
-class PluginInstaller extends LibraryInstaller {
+class PluginInstaller extends LibraryInstaller
+{
 
-/**
- * {@inheritDoc}
- */
-	public function supports($packageType) {
-		return 'cakephp-plugin' === $packageType;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return 'cakephp-plugin' === $packageType;
+    }
 
-/**
- * {@inheritDoc}
- *
- * @throws \RuntimeException
- */
-	public function getPackageBasePath(PackageInterface $package) {
-		$extra = $package->getExtra();
-		if (!empty($extra['installer-name'])) {
-			return 'plugins/' . $extra['installer-name'];
-		}
+    /**
+     * {@inheritDoc}
+     *
+     * @throws \RuntimeException
+     */
+    public function getPackageBasePath(PackageInterface $package)
+    {
+        $extra = $package->getExtra();
+        if (!empty($extra['installer-name'])) {
+            return 'plugins/' . $extra['installer-name'];
+        }
 
-		$primaryNS = null;
-		$autoLoad = $package->getAutoload();
-		foreach ($autoLoad as $type => $pathMap) {
-			if ($type !== 'psr-4') {
-				continue;
-			}
-			$count = count($pathMap);
+        $primaryNS = null;
+        $autoLoad = $package->getAutoload();
+        foreach ($autoLoad as $type => $pathMap) {
+            if ($type !== 'psr-4') {
+                continue;
+            }
+            $count = count($pathMap);
 
-			if ($count === 1) {
-				$primaryNS = key($pathMap);
-				break;
-			}
+            if ($count === 1) {
+                $primaryNS = key($pathMap);
+                break;
+            }
 
-			$matches = preg_grep('#^(\./)?src/?$#', $pathMap);
-			if ($matches) {
-				$primaryNS = key($matches);
-				break;
-			}
+            $matches = preg_grep('#^(\./)?src/?$#', $pathMap);
+            if ($matches) {
+                $primaryNS = key($matches);
+                break;
+            }
 
-			foreach (['', '.'] as $path) {
-				$key = array_search($path, $pathMap, true);
-				if ($key !== false) {
-					$primaryNS = $key;
-				}
-			}
-			break;
-		}
+            foreach (['', '.'] as $path) {
+                $key = array_search($path, $pathMap, true);
+                if ($key !== false) {
+                    $primaryNS = $key;
+                }
+            }
+            break;
+        }
 
-		if (!$primaryNS) {
-			throw new \RuntimeException(
-				"Unable to get plugin name. Ensure you have added proper 'autoload' section
+        if (!$primaryNS) {
+            throw new \RuntimeException(
+                "Unable to get plugin name. Ensure you have added proper 'autoload' section
 				to your plugin's config as stated in README on https://github.com/cakephp/plugin-installer"
-			);
-		}
+            );
+        }
 
-		return 'plugins/' . trim(str_replace('\\', '/', $primaryNS), '/');
-	}
-
+        return 'plugins/' . trim(str_replace('\\', '/', $primaryNS), '/');
+    }
 }
